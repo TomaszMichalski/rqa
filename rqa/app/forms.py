3ada9de7b8dd555705d74a296c0b2ea78e2ea449
TomaszMichalski <tomasz.michalski19@gmail.com>
2019-04-28 15:48:49 +0200

    Default value for PM is now True

--- rqa/app/forms.py
+++ rqa/app/forms.py
@@ -6,7 +6,7 @@ class GenerateForm(forms.Form):
     address = forms.CharField(label='Adres')
     date_from = forms.DateTimeField(label='Od')
     date_to = forms.DateTimeField(label='Do')
-    is_pm1 = forms.BooleanField(label='PM1', required=False)
-    is_pm25 = forms.BooleanField(label='PM2.5', required=False)
-    is_pm10 = forms.BooleanField(label='PM10', required=False)
+    is_pm1 = forms.BooleanField(label='PM1', required=False, initial=True)
+    is_pm25 = forms.BooleanField(label='PM2.5', required=False, initial=True)
+    is_pm10 = forms.BooleanField(label='PM10', required=False, initial=True)
 
