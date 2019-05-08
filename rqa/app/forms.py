0d872f3b76353b03d8e1e93914e913ec47c5cf06
TomaszMichalski <tomasz.michalski19@gmail.com>
2019-05-08 19:48:34 +0200

    Labels are now in English

--- rqa/app/forms.py
+++ rqa/app/forms.py
@@ -3,16 +3,16 @@ from . import models
 from datetime import datetime
 
 class GenerateForm(forms.Form):
-    address = forms.CharField(label='Adres')
-    radius = forms.CharField(label='Promień (km)')
-    date_from = forms.DateTimeField(label='Od')
-    date_to = forms.DateTimeField(label='Do')
+    address = forms.CharField(label='Address')
+    radius = forms.CharField(label='Radius (km)')
+    date_from = forms.DateTimeField(label='From')
+    date_to = forms.DateTimeField(label='To')
     is_pm1 = forms.BooleanField(label='PM1', required=False, initial=True)
     is_pm25 = forms.BooleanField(label='PM2.5', required=False, initial=True)
     is_pm10 = forms.BooleanField(label='PM10', required=False, initial=True)
-    is_temp = forms.BooleanField(label='Temperatura', required=False, initial=True)
-    is_pressure = forms.BooleanField(label='Ciśnienie', required=False, initial=True)
-    is_humidity = forms.BooleanField(label='Wilgotność', required=False, initial=True)
-    is_wind = forms.BooleanField(label='Wiatr', required=False, initial=True)
-    is_clouds = forms.BooleanField(label='Zachmurzenie', required=False, initial=True)
+    is_temp = forms.BooleanField(label='Temperature', required=False, initial=True)
+    is_pressure = forms.BooleanField(label='Pressure', required=False, initial=True)
+    is_humidity = forms.BooleanField(label='Humidity', required=False, initial=True)
+    is_wind = forms.BooleanField(label='Wind', required=False, initial=True)
+    is_clouds = forms.BooleanField(label='Clouds', required=False, initial=True)
 
