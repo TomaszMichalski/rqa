8f6621bc901c22d728712cef9a38ef02be6a4d3e
TomaszMichalski <tomasz.michalski19@gmail.com>
2019-05-28 18:07:04 +0200

    Basic user authentication

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class GenerationParameters():
     def __init__(self, address, radius, date_from, date_to, is_pm1, is_pm25, is_pm10, is_temp, is_pressure, is_humidity, is_wind, is_clouds):
@@ -26,3 +27,25 @@ class Installation():
 
     def __str__(self):
         return "[id: %f, lat: %f, lon: %f]" % (self.id, self.lat, self.lon)
+
+class Configuration(models.Model):
+    address = models.CharField(max_length=128)
+    radius = models.CharField(max_length=8)
+    period = models.DurationField()
+    is_pm1 = models.BooleanField()
+    is_pm25 = models.BooleanField()
+    is_pm10 = models.BooleanField()
+    is_temp = models.BooleanField()
+    is_pressure = models.BooleanField()
+    is_humidity = models.BooleanField()
+    is_wind = models.BooleanField()
+    is_clouds = models.BooleanField()
+
+class Group(models.Model):
+    name = models.CharField(max_length=64)
+    configuration = models.OneToOneField(Configuration, null=True, on_delete=models.SET_NULL)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    configuration = models.OneToOneField(Configuration, null=True, on_delete=models.SET_NULL)
+    group = models.OneToOneField(Group, null=True, on_delete=models.SET_NULL)
--- app/views.py
+++ app/views.py
@@ -1,39 +1,79 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as django_login
 from . import forms
 from . import models
 from . import util
 from . import db
 import json
 
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        form = forms.RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            configuration = models.Configuration()
+            profile = models.Profile(user=user)
+            profile.save()
+            django_login(request, user)
+            return redirect('home')
+    else:
+        form = forms.RegisterForm()
+    return render(request, 'user/register.html', { 'form': form })
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    return render(request, 'app/login.html')
+
+@login_required(login_url='user/login')
+def logout(request):
+    return render(request, 'app/logout.html')
+
+@login_required(login_url='user/login')
 def home(request):
     return render(request, 'app/home.html')
 
+@login_required(login_url='user/login')
 def analysis(request):
     return render(request, 'app/analysis.html')
 
+@login_required(login_url='user/login')
 def prediction(request):
     return render(request, 'app/prediction.html')
 
+@login_required(login_url='user/login')
 def configuration(request):
     return render(request, 'app/configuration.html')
 
+@login_required(login_url='user/login')
 def analysis_generate(request):
     form = forms.GenerateForm()
 
     return render(request, 'app/analysis_generate.html', { 'form': form })
 
+@login_required(login_url='user/login')
 def analysis_user(request):
     data = dict()
     info = []
 
     return render(request, 'app/analysis_chart.html', { 'data': data, 'info': info })
 
+@login_required(login_url='user/login')
 def analysis_group(request):
     data = dict()
     info = []
 
     return render(request, 'app/analysis_chart.html', { 'data': data, 'info': info })
 
+@login_required(login_url='user/login')
 def analysis_custom(request):
     form = forms.GenerateForm(request.GET)
     if form.is_valid():
@@ -46,23 +86,27 @@ def analysis_custom(request):
         
     return render(request, 'app/analysis_generate.html', { 'form': form })
 
+@login_required(login_url='user/login')
 def prediction_generate(request):
     form = forms.GenerateForm()
 
     return render(request, 'app/prediction_generate.html', { 'form': form })
 
+@login_required(login_url='user/login')
 def prediction_user(request):
     data = dict()
     info = []
 
     return render(request, 'app/prediction_chart.html', { 'data': data, 'info': info })
 
+@login_required(login_url='user/login')
 def prediction_group(request):
     data = dict()
     info = []
 
     return render(request, 'app/prediction_chart.html', { 'data': data, 'info': info })
 
+@login_required(login_url='user/login')
 def prediction_custom(request):
     form = forms.GenerateForm(request.GET)
     if form.is_valid():
