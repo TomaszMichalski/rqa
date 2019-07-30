eefabe3d0492643fd75c325a6237c95d266a3a5e
Dominik Dyduch <dominik.dyduch@sabre.com>
2019-07-30 23:41:29 +0200

    create and connect local psql

--- dbservice/worker.py
+++ dbservice/worker.py
@@ -14,10 +14,9 @@ def main():
 
     # local version
     if db_url is None:
-        proc = subprocess.Popen('heroku config:get DATABASE_URL -a rqa', stdout=subprocess.PIPE, shell=True)
-        db_url = proc.stdout.read().decode('utf-8').strip() + '?sslmode=require'
-
-    conn = psycopg2.connect(db_url)
+        conn = psycopg2.connect(host='localhost', user='rqa_user', password='password', dbname='rqa_db')
+    else:
+        conn = psycopg2.connect(db_url)
     cur = conn.cursor()
 
     retrieve_and_insert_readings_for_all_addresses(cur)
