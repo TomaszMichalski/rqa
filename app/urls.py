ee5e29dee53567b0e9c43c929690eec5fa2283ad
Dominik Dyduch <dominik.dyduch@gmail.com>
2019-09-08 22:38:11 +0200

    Email (#22)
    * settings for email
    
    * base email function
    
    * email with html message
    
    * send email for analysis and predictions
    
    * look prettier

--- app/urls.py
+++ app/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('user/login', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('user/logout', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('guest/generate', views.guest_generate, name='guest_generate'),
+    path('analysis/sendmail', views.send_analysis_email, name='send_analysis_email'),
+    path('prediction/sendmail', views.send_prediction_email, name='send_prediction_email'),
 ]
